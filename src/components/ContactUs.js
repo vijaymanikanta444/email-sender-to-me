4dffddaa6bba57415b259b92c0ca38f3e6d74e67
vijaymanikanta444 <vijayakp444@gmail.com>
2021-11-14 19:15:27 +0530

    email to me

---
+++ src/components/ContactUs.js
@@ -0,0 +1,39 @@
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_egha2bb',
+        'template_vctvszg',
+        e.target,
+        'user_BlHoEwxEueenrEAdriaUM'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
